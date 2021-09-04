e176f8083388fa7a3fd8cadea760291df53630a7
Nathan Williams <nate88williams@gmail.com>
2021-09-04 18:08:29 -0500

    Git initialized

---
+++ src/Project_1.java
@@ -0,0 +1,20 @@
+/************************************************************************************
+ * Project 1: Sieve of Erotosthenes
+ * Write a java program that computes all prime numbers less than or equal to
+ * some number N. The algorithm used is Sieve of Erotosthese.
+ * 
+ * Created on 9/4/21 By Nathan Williams
+ * Course: 3345.005
+ * @author nate
+ *
+************************************************************************************/
+
+
+public class Project_1 {
+
+	public static void main(String[] args) {
+		int[] intArray;
+
+	}
+
+}
