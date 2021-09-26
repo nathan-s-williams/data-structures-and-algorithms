eaf0af591dcf4d61412f8ae264e1a22e95634c1c
Nathan Williams <nate88williams@gmail.com>
2021-09-26 17:30:07 -0500

    Added additional methods to IDedLinkedList class.

--- src/IDedLinkedList.java
+++ src/IDedLinkedList.java
@@ -1,6 +1,9 @@
 
 public class IDedLinkedList<T extends IDedObject> {
 	
+	private Node<T> head;
+	private Node<T> tail;
+	private int size;
 	
 	private static class Node<T extends IDedObject> {
 		T data;
@@ -17,16 +20,31 @@ public class IDedLinkedList<T extends IDedObject> {
 	}
 	
 	public void makeEmpty() {
+		head = new Node<T>(null, null);
+		tail = new Node<T>(null, null);
+		size = 0;
+	}
+	
+	public T findID(int x) {
+		
+		
+		
+		return null; //placeholder
 		
 	}
 	
 	public boolean insertAtFront(T x) {
-		return false; //placeholder
+		Node<T> NewNode = new Node<T>(x, head);
+		head = NewNode;
+		
+		return true;
 	}
 	
 	public T deleteFromFront() {
-		T x = null;
-		return x; //placeholder
+		Node<T> deletedNode = head;
+		head = new Node<T>(head.next.data,head.next.next);
+		
+		return deletedNode.data;
 	}
 	
 	T delete(int id) {
