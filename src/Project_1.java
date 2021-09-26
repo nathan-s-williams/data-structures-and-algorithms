c3a89b195554744cb4f189f982d80a56b33c8c2b
Nathan Williams <nate88williams@gmail.com>
2021-09-26 12:41:26 -0500

    Update to Project_1 file.

--- src/Project_1.java
+++ src/Project_1.java
@@ -3,8 +3,8 @@
  * Write a java program that computes all prime numbers less than or equal to
  * some number N. The algorithm used is Sieve of Erotosthese.
  * 
- * Created on 9/4/21 By Nathan Williams
- * Course: 3345.005
+ * Created on 9/4/21 By Nathan Williams (nsw200000)
+ * Course: 3345.005 Data Structures and Introduction to Algorithmic Analysis
  * @author nate
  *
 ************************************************************************************/
@@ -12,27 +12,35 @@
 import java.util.*;
 
 public class Project_1 {
-	//static int N;						//Declare variable for user input.
+	
 	public static void main(String[] args) {
+		//Declare scanner object, int variable for input and boolean variable for input validation.
 		Scanner console = new Scanner(System.in);
 		int N = 0;							
-		boolean inputError;				//Declare variable for input validation.
+		boolean inputError;
 		
-		System.out.print("Please enter a positive integer value for which the program will "
-				+ "find all primes up to the specified number: ");
+		//Prompt user for input.
+		System.out.print("Please enter an integer value greater than 2 and the program will "
+				+ "find all primes up to that number: ");
 		
-		//NOTE TO SELF. CHECK IF NUMBER IS > 1!!!!!!!!
+		//Loop that collects user input. If input validation fails prompt user to reenter integer.
 		do {
 			inputError = false;
 			try {
 				N = console.nextInt();
+				//Check if input is greater than or equal to 2. If not, prompt user again and clear buffer. 
+				//Set inputError bool variable to true
+				if(N < 2) {
+					System.out.print("\nInvalid input. Please ensure that you enter an integer value greater than or equal to 2: ");
+					console.nextLine();
+					inputError = true;
+				}
 			}
+			//Catch input mismatch. Prompt user again and clear buffer. Set inputError bool variable to true.
 			catch(InputMismatchException e) {
-				System.out.println("\n\nInvalid input. Please ensure that you enter a positive"
-						+ " integer.");
-				System.out.print("Please enter another integer value: ");
-				console.nextLine();		//Force buffer to move to next line in preparation for new input.
-				inputError = true;		//Set inputError to true for loop to return to beginning.
+				System.out.print("\nInvalid input. Please ensure that you enter an integer value greater than or equal to 2: ");
+				console.nextLine();
+				inputError = true;
 			}
 		} while(inputError);
 		
@@ -43,22 +51,20 @@ public class Project_1 {
 													//consider them in this algorithm.
 		}
 		
-		
-		int sqrtN = ((int) Math.pow(N,0.5));		//Create for loop max length
-		for(int i = 2; i <= sqrtN; i++) {
-			if(boolArray[i]) {
+		int sqrtN = ((int) Math.pow(N,0.5));		//Set loop max variable equal to sqrt of N.
+		for(int i = 2; i <= sqrtN; i++) {			//Loop through array starting at 2 to sqrt(N).
+			if(boolArray[i]) {						//If boolArray index value is true move to nested loop.
+													//Nested loop will set all i^2 + i integers to false.
 				for(int j = (int) Math.pow(i,2);j < boolArray.length; j += i) {
 					boolArray[j] = false;
 				}
 			}
 		}
 		
+		//Print all integers in array that have their bool values set to true.
 		for(int i = 2; i < boolArray.length; i++) {
 			if(boolArray[i])
 				System.out.printf("%d ", i);
 		}
-		
-
 	}
-
 }
