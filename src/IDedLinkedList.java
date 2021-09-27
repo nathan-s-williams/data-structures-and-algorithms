f31710e9bd4d35882e5470c3853dab0a300f7a21
Nathan Williams <nate88williams@gmail.com>
2021-09-27 06:17:15 -0500

    First version of IDedLinkedList completed. Ready for testing.

--- src/IDedLinkedList.java
+++ src/IDedLinkedList.java
@@ -3,6 +3,8 @@ public class IDedLinkedList<T extends IDedObject> {
 	
 	private Node<T> head;
 	private Node<T> tail;
+	private Node<T> searchNode;
+	private Node<T> searchPreviousNode;
 	private int size;
 	
 	private static class Node<T extends IDedObject> {
@@ -22,37 +24,80 @@ public class IDedLinkedList<T extends IDedObject> {
 	public void makeEmpty() {
 		head = new Node<T>(null, null);
 		tail = new Node<T>(null, null);
+		head.next = tail;
 		size = 0;
 	}
 	
+	public boolean isEmpty() {
+		if(size == 0) 
+			return true;
+		
+		return false;
+	}
+	
 	public T findID(int x) {
+		if(size == 0)
+			return null;
 		
+		searchNode = head;
 		
+		while(searchNode.data.getID() != x && searchNode != null) {
+			searchPreviousNode = searchNode;
+			searchNode = searchNode.next;
+		}
 		
-		return null; //placeholder
+		if(searchNode != null)
+			return searchNode.data;
+		return null;
 		
 	}
 	
 	public boolean insertAtFront(T x) {
-		Node<T> NewNode = new Node<T>(x, head);
-		head = NewNode;
+		if(findID(x.getID()) != null)
+			return false;
 		
+		if(isEmpty()) {
+			head.data = x;
+			
+		}
+		else {
+			Node<T> NewNode = new Node<T>(x, head);
+			head = NewNode;	
+		}
+		
+		size++;
 		return true;
 	}
 	
 	public T deleteFromFront() {
+		if(isEmpty())
+			return null;
+		
 		Node<T> deletedNode = head;
 		head = new Node<T>(head.next.data,head.next.next);
 		
+		size--;
 		return deletedNode.data;
 	}
 	
 	T delete(int id) {
-		T x = null;
-		return x; //placeholder
+		if(isEmpty())
+			return null;
+		
+		if(findID(id) == null)
+			return null;
+
+		if(searchPreviousNode == null)
+			return deleteFromFront();	//Node is at head. 
+		
+		searchPreviousNode.next = searchNode.next;
+		size--;
+		return searchNode.data;
 	}
 	
 	public int printTotal() {
-		return 0; //placeholder
+		if(isEmpty())
+			return -1;
+		return size;
 	}
 }
