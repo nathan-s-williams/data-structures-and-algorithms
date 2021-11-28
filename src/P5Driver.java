7994d7e243e37d75a2a598ea206491aba351f262
Nathan Williams <nate88williams@gmail.com>
2021-11-28 11:54:18 -0600

    Added driver class for QuickSorter.java.

---
+++ src/P5Driver.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class P5Driver {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> list;
+		list = QuickSorter.generateRandomList(100);
+		
+		System.out.print(list.toString());
+
+	}
+
+}
