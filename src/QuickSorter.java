66a61f9beba3cd3224bbc9595cab871e27b36e15
Nathan Williams <nate88williams@gmail.com>
2021-11-28 11:52:43 -0600

    Updated and added the following: -PivotSrategy -timedQuickSort -generateRandomList

--- src/QuickSorter.java
+++ src/QuickSorter.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.*;
 
 /************************************************************************************************
  * Project 5:
@@ -10,7 +11,7 @@ import java.util.ArrayList;
  * extends java.lang.Comparable<E>
  ************************************************************************************************/
 
-public class QuickSorter<E>{
+public class QuickSorter{
 	
 	public enum PivotStategy{
 		FIRST_ELEMENT,
@@ -19,11 +20,24 @@ public class QuickSorter<E>{
 		MEDIAN_OF_THREE
 	}
 	
-	public static <E extends java.lang.Comparable<E>> java.time.Duration timedQuickSort(ArrayList<E> list, PivotStategy pivotStrategy) {
+	public static <E extends java.lang.Comparable<E>> java.time.Duration timedQuickSort(ArrayList<E> list, PivotStategy pivotStrategy) throws NullPointerException {
 		java.time.Duration test;
 		return null;
 		
 		
+	}
+	
+	public static ArrayList<Integer> generateRandomList(int size) throws IllegalArgumentException{
+		if(size < 0)
+			throw new IllegalArgumentException();
+		
+		ArrayList<Integer> unsortedList = new ArrayList<>(size);
+		Random rand = new Random();
+		for(int i = 0; i < size; i++) {
+			unsortedList.add(rand.nextInt());
+		}
+		
+		return unsortedList;
 	}
 	
 }
